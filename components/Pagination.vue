556d6d52770c96a3597420b17ffc338c35891e84
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-07-04 10:27:48 -0400

    update pagination component

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,52 +1,45 @@
 <template>
   <div class="containerBtn">
-    <button
-        class="nextPrev"
-        type="button"
-        v-if="page != 1"
-        @click="page--"
-    ><img src="~/static/icons/arrowClosed.svg" class="icon-prev"/></button>
+    <button class="nextPrev" type="button" v-if="page != 1" @click="page--"><img src="~/static/icons/arrowClosed.svg"
+        class="icon-prev" /></button>
 
     <div class="rowsBtn">
-      <button
-          class="btn-pagination"
-          type="button"
-          v-for="pageNumber in pages.slice(page - 1, page + 5)"
-          :key="pageNumber"
-          @click="page = pageNumber"
-      >
+      <button class="btn-pagination" type="button" v-for="pageNumber in pages.slice(page - 1, page + 5)"
+        :key="pageNumber" @click="page = pageNumber">
         {{ pageNumber }}
       </button>
     </div>
-      
-    <button
-        class="nextPrev"
-        type="button"
-        @click="page++"
-        v-if="page < pages.length"
-    ><img src="~/static/icons/arrowClosed.svg" class="icon-next"/></button>
+
+    <button class="nextPrev" type="button" @click="page++" v-if="page < pages.length"><img
+        src="~/static/icons/arrowClosed.svg" class="icon-next" /></button>
   </div>
 </template>
 
 <script>
 export default {
-  props:['list'],
+  props: ['list'],
   emits: ['displayedPosts'],
 
-  data(){
-    return{
-      posts: [""],
+  data() {
+    return {
+      posts: [],
       page: 1,
       perPage: 10,
       pages: [],
     }
   },
-  created: async function (){
-    this.posts = this.list
+  created() {
+    setTimeout( () => {
+      this.posts = this.list
+      console.log("2 -> pagination render");
+    }, 1000)
+    
+  
   },
 
   computed: {
     displayedPosts() {
+      console.log("testeeee");
       return this.paginate(this.posts)
     },
   },
@@ -68,7 +61,7 @@ export default {
   },
 
   watch: {
-    displayedPosts(newValue){
+    displayedPosts(newValue) {
       this.$emit('displayNewList', newValue)
     },
     posts() {
@@ -80,20 +73,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.containerBtn{
+.containerBtn {
   margin-top: 1rem;
   display: flex;
   gap: .5rem;
 
-  .rowsBtn{
+  .rowsBtn {
     background-color: var(--gray);
     width: fit-content;
     border-radius: 2rem;
     padding: 0rem 1.5rem;
-    
+
   }
 
-  .nextPrev{
+  .nextPrev {
     border-radius: 50%;
     min-width: 40px;
     width: 40px;
@@ -101,10 +94,12 @@ export default {
     .icon-prev {
       transform: rotate(-90deg);
     }
+
     .icon-next {
       transform: rotate(90deg);
     }
   }
+
   .btn-pagination {
     cursor: pointer;
     min-width: 40px;
@@ -117,7 +112,7 @@ export default {
     border-radius: 50%;
     background-color: var(--gray);
   }
-  
+
   .btn-pagination:focus {
     background-color: var(--gray_text);
     color: var(--white);
