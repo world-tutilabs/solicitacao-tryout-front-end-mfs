002fd95e58403a5970752ec6a877b99c5ebb3461
Bruno_Marinho <bcaldasmarinhojklb@gmail.com>
2022-07-07 08:37:56 -0300

    toats

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    "vue-toastification/nuxt"
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
