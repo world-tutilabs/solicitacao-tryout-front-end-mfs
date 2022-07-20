0b78650e1277dfb5dec72056086a11e05bf8ad83
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-07-20 10:19:27 -0400

    update token

--- services/config/index.js
+++ services/config/index.js
@@ -13,7 +13,7 @@ const http = axios.create({
 http.interceptors.request.use(function (config) {
 
 
-  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtYXRyaWN1bGEiOiI1MDU0IiwiaWF0IjoxNjU4MjU4NjU4LCJleHAiOjE2NTgyODc0NTgsInN1YiI6ImEzMDZmYTVmLTJmZmItNDMwNi05YmY2LWExNzg1YTY3OThjZiJ9.Gy5ycHzfKs4YGtsZqDt57po7QnRPtRcj4iYc1t_QtR8';
+  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtYXRyaWN1bGEiOiI1MDU0IiwiaWF0IjoxNjU4MzIxNzgyLCJleHAiOjE2NTgzNTA1ODIsInN1YiI6ImEzMDZmYTVmLTJmZmItNDMwNi05YmY2LWExNzg1YTY3OThjZiJ9.ZZoUeTWhfkNUhW_7H-9bTrvbuoWjhbCLjdjOkStvz5M';
 
 
 
