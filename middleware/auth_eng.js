66c4522f1e2bfcd0f3de90148dc2c90ac3f02c36
Luan Albuquerque <luan.santos6605@gmail.com>
2022-11-10 17:00:26 -0400

    refactor: teste

--- middleware/auth_eng.js
+++ middleware/auth_eng.js
@@ -1,34 +1,32 @@
-import Cookies from "js-cookie"
+import { http } from "~/services/config";
 
 export default async function ({ redirect }) {
 
   let user;
   try {
-    const res = await axios.post(`${process.env.ROUTER_VERIFY_USER}`,
-    {},
-    { headers: { Authorization: `${Cookies.get('auth._token.local')}` } });
 
+    const res = await http.post(process.env.ROUTER_VERIFY_USER);
     user = res.data.user
+
 
   } catch (e) {
     // Cookies.set('auth._token.local', false);
     // Cookies.set('auth._token_expiration.local', false);
     // return redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`)
-
+    console.log("entrou");
   }
 
   if (user.nivel_de_acesso.descricao === "eng_analista" ||
-      user.nivel_de_acesso.descricao === "eng_admin" ||
-      user.nivel_de_acesso.descricao === "eng"
+  user.nivel_de_acesso.descricao === "eng_admin" ||
+  user.nivel_de_acesso.descricao === "eng"
 ) {
 
   return user
+
+} else {
+  return redirect(`${process.env.ROUTER_SYSTEM_PCP}`)
+
+
 }
-
-console.log(user)
-// } else {
-//   return redirect(`${process.env.ROUTER_SYSTEM_PCP}`)
-
-// }
 
 }
--- middleware/auth_pcp.js
+++ middleware/auth_pcp.js
@@ -1,14 +1,11 @@
-
-import Cookies from "js-cookie"
+import { http } from "~/services/config";
 
 export default async function({redirect}){
   let user;
     try {
-      const res = await axios.post(`${process.env.ROUTER_VERIFY_USER}`,
-      {},
-      { headers: { Authorization: `${Cookies.get('auth._token.local')}` } });
-
-      user = res.data.user
+       const res =  await http.post(process.env.ROUTER_VERIFY_USER);
+        user = res.data.user
+      // console.log(res);
 
         } catch (e) {
          //   Cookies.set('auth._token.local', false);
@@ -19,20 +16,19 @@ export default async function({redirect}){
         }
 
 
-        if (
-           user.nivel_de_acesso.descricao === "pcp_acabamento"
+        if (user.nivel_de_acesso.descricao === "pcp_acabamento"
         || user.nivel_de_acesso.descricao === "pcp_injecao"
         || user.nivel_de_acesso.descricao === "pcp") {
-
-          return user;
-
-        } else if (
-          user.nivel_de_acesso.descricao === "eng_analista" ||
-          user.nivel_de_acesso.descricao === "eng_admin" ||
-          user.nivel_de_acesso.descricao === "eng"
+           return user;
+        } else if (user.nivel_de_acesso.descricao === "eng_analista" ||
+        user.nivel_de_acesso.descricao === "eng_admin" ||
+        user.nivel_de_acesso.descricao === "eng"
       ) {
        return redirect(`${process.env.ROUTER_SYSTEM_ENG}`)
 
-      }
+      } else {
+         return   redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`)
+
+        }
 
 }
