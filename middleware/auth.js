28593c9793e07b1aa9cbaa7b450beeea1e2d79ce
Luan Albuquerque <luan.santos6605@gmail.com>
2022-11-10 16:40:46 -0400

    refactor: mudando middlwares

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,17 +7,17 @@ export default async function ({ redirect, app, store }) {
 
     try {
 
-      await axios.post(`${process.env.ROUTER_VERIFY_USER}`,
+     const res = await axios.post(`${process.env.ROUTER_VERIFY_USER}`,
         {},
         { headers: { Authorization: `${Cookies.get('auth._token.local')}` } });
 
-
+     console.log({res});
      return;
 
       } catch (e) {
         //  Cookies.set('auth._token.local', false);
         //  Cookies.set('auth._token_expiration.local', false);
-       return  redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`)
+      //  return  redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`)
       // return redirect(`http://192.168.2.9:8500/`)
 
       }
