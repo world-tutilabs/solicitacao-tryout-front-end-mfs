d4c90ef9654fa0c162ec0fe94eb7acebc175d5fd
Luan Albuquerque <luan.santos6605@gmail.com>
2022-11-11 07:46:15 -0400

    refactor: realizando middleware

--- middleware/auth_eng.js
+++ middleware/auth_eng.js
@@ -1,32 +1,42 @@
-import { http } from "~/services/config";
+
+import axios from "axios"
+import Cookies from "js-cookie"
 
 export default async function ({ redirect }) {
 
   let user;
-  try {
 
-    const res = await http.post(process.env.ROUTER_VERIFY_USER);
+  await axios
+  .post(
+    `${process.env.ROUTER_VERIFY_USER}`,
+    {},
+    { headers: { Authorization: `${Cookies.get("auth._token.local")}` } }
+  )
+  .then((res) => {
     user = res.data.user
 
+    if (user.nivel_de_acesso.descricao === "eng_analista" ||
+    user.nivel_de_acesso.descricao === "eng_admin" ||
+    user.nivel_de_acesso.descricao === "eng"
+      ) {
 
-  } catch (e) {
-    // Cookies.set('auth._token.local', false);
-    // Cookies.set('auth._token_expiration.local', false);
-    // return redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`)
-    console.log("entrou");
+    return user
+
+  } else if (
+    user.nivel_de_acesso.descricao === "pcp_acabamento" ||
+    user.nivel_de_acesso.descricao === "pcp_injecao" ||
+    user.nivel_de_acesso.descricao === "pcp"
+  )  {
+
+    return redirect(`${process.env.ROUTER_SYSTEM_PCP}`)
+
   }
 
-  if (user.nivel_de_acesso.descricao === "eng_analista" ||
-  user.nivel_de_acesso.descricao === "eng_admin" ||
-  user.nivel_de_acesso.descricao === "eng"
-) {
-
-  return user
-
-} else {
-  return redirect(`${process.env.ROUTER_SYSTEM_PCP}`)
+  }).catch((e)=>{
+    console.log(e);
+    // return redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`);
+  })
 
 
-}
 
 }
--- middleware/auth_pcp.js
+++ middleware/auth_pcp.js
@@ -1,34 +1,38 @@
 import { http } from "~/services/config";
+import axios from "axios"
+import Cookies from "js-cookie"
 
-export default async function({redirect}){
+export default async function ({ redirect }) {
   let user;
-    try {
-       const res =  await http.post(process.env.ROUTER_VERIFY_USER);
-        user = res.data.user
-      // console.log(res);
 
-        } catch (e) {
-         //   Cookies.set('auth._token.local', false);
-         //   Cookies.set('auth._token_expiration.local', false);
-          return  redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`)
-      // return redirect(`http://192.168.2.9:8500/`)
-
-        }
-
-
-        if (user.nivel_de_acesso.descricao === "pcp_acabamento"
-        || user.nivel_de_acesso.descricao === "pcp_injecao"
-        || user.nivel_de_acesso.descricao === "pcp") {
-           return user;
-        } else if (user.nivel_de_acesso.descricao === "eng_analista" ||
+  await axios
+    .post(
+      `${process.env.ROUTER_VERIFY_USER}`,
+      {},
+      { headers: { Authorization: `${Cookies.get("auth._token.local")}` } }
+    )
+    .then((res) => {
+      user = res.data.user;
+      if (
+        user.nivel_de_acesso.descricao === "pcp_acabamento" ||
+        user.nivel_de_acesso.descricao === "pcp_injecao" ||
+        user.nivel_de_acesso.descricao === "pcp"
+      ) {
+        return user;
+      } else if (
+        user.nivel_de_acesso.descricao === "eng_analista" ||
         user.nivel_de_acesso.descricao === "eng_admin" ||
         user.nivel_de_acesso.descricao === "eng"
       ) {
-       return redirect(`${process.env.ROUTER_SYSTEM_ENG}`)
-
+        return redirect(`${process.env.ROUTER_SYSTEM_ENG}`);
       } else {
-         return   redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`)
-
-        }
-
+        return redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`);
+      }
+    })
+    .catch((e) => {
+      //   Cookies.set('auth._token.local', false);
+      //   Cookies.set('auth._token_expiration.local', false);
+      // return redirect(`http://192.168.2.9:8500/`)
+      return redirect(`${process.env.ROUTER_REDIRECT_SYSTEM_USER}`);
+    });
 }
