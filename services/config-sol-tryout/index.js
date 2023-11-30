677088a023206780d49af4eb76c222006a72630a
yan-levy <yanbatistacel@gmail.com>
2023-11-30 09:58:04 -0400

    💚 fix: alterando baseURLs de locais para URL do servidor

--- services/config-sol-tryout/index.js
+++ services/config-sol-tryout/index.js
@@ -1,39 +1,38 @@
-import axios from "axios"
-import Cookies from 'js-cookie'
+import axios from "axios";
+import Cookies from "js-cookie";
 
 // servidor
+const httpSolTryOut = axios.create({
+  baseURL: `http://185.209.179.253:5000/api`, //Adicionar Base URL
+  headers: {
+    Accept: "application/json",
+    Content: "application/json",
+  },
+});
+
+// local
 // const httpSolTryOut = axios.create({
-//   baseURL: `http://185.209.179.253:5000/api`,     //Adicionar Base URL
+//   baseURL: `http://172.21.3.192:3000/api`,     //Adicionar Base URL
 //   headers: {
 //     "Accept": "application/json",
 //     "Content": "application/json"
 //   }
 // })
 
-// local
-const httpSolTryOut = axios.create({
-  baseURL: `http://172.21.3.192:3000/api`,     //Adicionar Base URL
-  headers: {
-    "Accept": "application/json",
-    "Content": "application/json"
-  }
-})
+httpSolTryOut.interceptors.request.use(
+  function (config) {
+    const token = Cookies.get("auth._token.local");
+    // const token = `${process.env.TOKEN_LOCAL}`
+    if (!token) {
+      token = "";
+    }
 
-httpSolTryOut.interceptors.request.use(function (config) {
-  const token = Cookies.get('auth._token.local')
-  // const token = `${process.env.TOKEN_LOCAL}`
-  if (!token) {
-    token = "";
-  }
-
-  config.headers.Authorization = `${token}`;
-  return config;
-},
+    config.headers.Authorization = `${token}`;
+    return config;
+  },
   (error) => {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-
-
-export { httpSolTryOut }
+export { httpSolTryOut };
